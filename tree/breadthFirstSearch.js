ca42d62243492aead0d9b758da8a46f1e6301006
duanxuezhi <794465732@qq.com>
2021-06-02 18:33:10 +0800

    chore：leetcode 102

--- tree/breadthFirstSearch.js
+++ tree/breadthFirstSearch.js
@@ -55,3 +55,35 @@ const a = function(root) {
     if (n.right) q.push([ n.right, l + 1 ]);
   }
 }
+
+const b = function(root) {
+  if (!root) return [];
+  const q = [[root, 0]];
+  let res = [];
+  while (q.length > 0) {
+    const [n, l] = q.shift();
+    if (!res[l]) res[l] = [];
+    res[l].push(n.val);
+    if (n.left) q.push([n.left, l + 1]);
+    if (n.right) q.push([n.right, l + 1]);
+  }
+  return res;
+}
+
+// 时间复杂度O(n) n：树的节点；空间复杂度：O(n)
+const b2 = function(root) {
+  if (!root) return [];
+  const q = [root]; // [整个树, 其他值]，树是其中一个数组项
+  const res = [];
+  while (q.length) { // 开始是以根元素为头，q数组仅一个长度
+    let len = q.length // 记录数组当前长度，避免重复使用时计算新的长度
+    res.push([]) // 初始化最后一个数组
+    while (len--) {
+      const n = q.shift();
+      res[res.length - 1].push(n.val); // 拿到最后一个数组
+      if (n.left) q.push(n.left); // 重复利用q数组
+      if (n.right) q.push(n.right); // 重复利用q数组
+    }
+  }
+  return res;
+}
