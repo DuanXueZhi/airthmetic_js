912442d8ee6832ec9039ce5cdd813f92abf26e39
KitaiGlory <794465732@qq.com>
2021-07-17 23:04:10 +0800

    fix：二分搜索 双指针必须有等于

--- sort&indexOf/search/binarySearch.js
+++ sort&indexOf/search/binarySearch.js
@@ -1,10 +1,10 @@
 // 时间复杂度：O(log N) 每次搜索都是原来的一半
 Array.prototype.binarySearch = function(target) {
-  this.sort(); // 必须是有序数组
+  // 必须是有序数组
   // 搜索的下标范围，因为要折半搜索
   let low = 0; // 数组的最‘小’下标
   let height = this.length - 1; // 数组搜索的最‘大’下标
-  while(low <= height) {
+  while(low <= height) { // 注意必须有等于
     const mid = (low + height) >> 1; // Math.floor((low + height) / 2);
     const element = this[mid]; // 中间元素
     if (element < target) { // 需要跳转到较大的那一半数组搜索
